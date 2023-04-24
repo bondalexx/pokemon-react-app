232feeebe57bc6a8db37dd5322a1dcf1d3c78b11
Alex Bondarenko <bond.alexx@gmail.com>
2023-04-25 01:00:31 +0300

    finall layout

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -12,15 +12,56 @@ const MainContent = () => {
 return(
     <main className="main">
         <div className="article">
-            <section className="img-section main-section">
-                <img className="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-                <FontAwesomeIcon icon={faArrowRight} size="2x"/>
-                <img className="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"/>
-                <FontAwesomeIcon icon={faArrowRight} size="2x" />
-                <img className="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"/>
-            </section>
+        <section className="img-section main-section">
+  <h1>Forms</h1>
+  <div className="card-container">
+    <div className="card">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
+      <p>Bulbasaur</p>
+    </div>
+    <FontAwesomeIcon icon={faArrowRight} size="2x"/>
+    <div className="card">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png" alt="ivysaur" />
+      <p>Ivysaur</p>
+    </div>
+    <FontAwesomeIcon icon={faArrowRight} size="2x"/>
+    <div className="card">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png" alt="venusaur" />
+      <p>Venusaur</p>
+    </div>
+  </div>
+</section>
+
             <section className="description-section main-section">
-              
+                <h1>Base stats</h1>
+                <div className="card-container">
+                    <div className="cards">
+                        <p>HP: 45</p>
+                        <p>Attack: 49</p>
+                        <p>Defense: 49</p>
+                        <p>Special-attack: 65</p>
+                        <p>Special-defense: 65</p>
+                        <p>Speed: 45</p>
+                    </div>
+                    <FontAwesomeIcon icon={faArrowRight} size="2x"/>
+                    <div className="cards">
+                        <p>HP: 45</p>
+                        <p>Attack: 49</p>
+                        <p>Defense: 49</p>
+                        <p>Special-attack: 65</p>
+                        <p>Special-defense: 65</p>
+                        <p>Speed: 45</p>
+                    </div>
+                    <FontAwesomeIcon icon={faArrowRight} size="2x"/>
+                    <div className="cards">
+                        <p>HP: 45</p>
+                        <p>Attack: 49</p>
+                        <p>Defense: 49</p>
+                        <p>Special-attack: 65</p>
+                        <p>Special-defense: 65</p>
+                        <p>Speed: 45</p>
+                    </div>
+                </div>
             </section>
             <section className="title-section main-section">
                 <h1 className="poke-name">
@@ -37,15 +78,6 @@ return(
             </div>
                 
             </section>
-        </div>
-        <div className="article">
-
-        </div>
-        <div className="article">
-
-        </div>
-        <div className="article">
-
         </div>
     </main>
 )
