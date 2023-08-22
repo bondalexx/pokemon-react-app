68a3e31cfa894713b8fcbe3b687d3bc257723a5c
Alex Bondarenko <bond.alexx@gmail.com>
2023-08-22 21:10:05 +0300

    solved bug with nav refrsh

--- src/components/pages/MainPage/mainPage.js
+++ src/components/pages/MainPage/mainPage.js
@@ -9,10 +9,20 @@ import "./mainPage.css";
 const MainPage = () => {
   const { getPokemons } = usePokemonsStore();
   const [activeIndex, setActiveIndex] = useState(0);
+  const updateStatus = () => {
+    for (let i = 1; i <= activeIndex; i++) {
+      const currentGroup = document.getElementById(`${i}`);
+      currentGroup.dataset.status = "unknown";
+    }
+    const nextGroup = document.getElementById(`${0}`);
+    nextGroup.dataset.status = "active";
+  };
   const onReturn = () => {
     getPokemons(0);
+    updateStatus();
     setActiveIndex(0);
   };
+
   return (
     <div className="pokemon-list">
       <Navbar onReturn={onReturn}></Navbar>
